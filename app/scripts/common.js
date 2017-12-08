53f28e620ce65730076966ecfabe5e31ba50886f
nicewahson <nicelj92@gmail.com>
2017-12-08 14:33:35 +0800

    x

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -99,11 +99,7 @@ function wxShareReady(lineLink, shareTitle, shareContent, shareLogo) {
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function (res) {
                 // 用户确认分享后执行的回调函数
-                alert(JSON.stringify(res));
-                alert('分享到朋友');
-                alert('')
-                alert('as======fd');
-                alert(sessionStorage+'as======fd');
+                alert(sessionStorage.getItem('accessinfo'));
             },
             cancel: function () {
                 // 用户取消分享后执行的回调函数
