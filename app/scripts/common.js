1c64ad624c822fad4a84ad571e545a26824e4dfc
nicewahson <nicelj92@gmail.com>
2017-12-09 11:27:32 +0800

    r

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -95,7 +95,7 @@ function wxShareReady(lineLink, shareTitle, shareContent, shareLogo) {
             imgUrl: shareLogo, // 分享图标
             success: function (res) {
                 // 用户确认分享后执行的回调函数
-                // updateShareTimes()
+                updateShareTimes()
             },
             cancel: function (res) {
                 // 用户取消分享后执行的回调函数
