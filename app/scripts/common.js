1a496a9d0b988442fc5ce131c2b3f0c41ec9c490
nicewahson <nicelj92@gmail.com>
2017-12-09 10:01:50 +0800

    r

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -114,10 +114,12 @@ function wxShareReady(lineLink, shareTitle, shareContent, shareLogo) {
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function (res) {
                 // 用户确认分享后执行的回调函数
-                updateShareTimes()
+                // updateShareTimes()
+                alert('fengxiangpengyou')
             },
             cancel: function () {
                 // 用户取消分享后执行的回调函数
+                alert('shibai')
             }
         });
 
